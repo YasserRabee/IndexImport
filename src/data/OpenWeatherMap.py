3d0b2515178addf059db7ba7364e2bf339111933
Andrew Morse <andrew@cascadallc.com>
2017-12-08 15:52:07 -0800

    Cleaning this up a bit

--- src/data/OpenWeatherMap.py
+++ src/data/OpenWeatherMap.py
@@ -9,19 +9,14 @@ location = ' '.join(sys.argv[1:])
 
 aapid='6b917d2932792806ba0594461b835275'
 
-url = 'http://api.openweathermap.org/data/2.5/weather?zip=%s,us&APPID=%s' % (location, aapid)
+unitsOfMeasure='metric'
+
+url = 'http://api.openweathermap.org/data/2.5/weather?zip=%s,us&units=%s&APPID=%s' % (location,unitsOfMeasure,aapid)
 response = requests.get(url)
 response.raise_for_status()
 
 weatherData = json.loads(response.text)
-w = weatherData
-print(w)
+print('Current weather location is:', weatherData['name'])
+print(weatherData)
 
-# print('Current weather is %s:' % (location))
-# print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
-# print()
-# print('Tomorrow:')
-# print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
-# print()
-# print('Day after tomorrow:')
-# print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
+
